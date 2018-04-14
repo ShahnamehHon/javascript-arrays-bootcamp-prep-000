4d77ec60ae03e6ad0ad39c80c61703a246e5db0f
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-14 21:47:08 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var addElementToEndOfArray = ['1', '2', '3'];
+["test", ...addElementToEndOfArray];
+addElementToEndOfArray();
